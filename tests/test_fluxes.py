56abae97d66b5c9e0528b44a027e0c306237e88f
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2024-02-07 12:54:22 -0600

    Testing updates
    - Switch test_fluxes to use parameterize instead of subtest
    - use tap output of test_fluxes so meson test understands the parameterized tests
    - add reports to github actions

--- tests/test_fluxes.py
+++ tests/test_fluxes.py
@@ -7,49 +7,46 @@ from pathlib import Path
 import nuflux
 import numpy as np
 import pytest
+from numpy.testing import assert_allclose
 
 with (Path(__file__).parent / "test_data.json").open() as f:
     data = json.load(f)
+models = [
+    (energy, model, knee)
+    for energy, v1 in data.items()
+    if energy != "doc"
+    for model, v2 in v1.items()
+    for knee in v2
+]
+coszeniths = [-1, -0.5, 0, 0.5, 1]
+low_energies = [1.0e00, 1.0e01, 1.0e02, 1.0e03]
+high_energies = np.logspace(2, 9, 8)
 
 
-def compare_fluxes(subtests, data, energies):
-    coszeniths = [-1, -0.5, 0, 0.5, 1]
-    E, cz = np.meshgrid(energies, coszeniths)
-
-    for model, x in data.items():
-        flux = nuflux.makeFlux(model)
-        emin, emax = flux.energy_range
-        geomE = (emin * emax) ** 0.5
-        for knee, y in x.items():
-            flux.knee_reweighting_model = knee
-            print("testing ", model, knee)
-            for nu_str, nu in nuflux.ParticleType.names.items():
-                with subtests.test(model=model, knee=knee, nu=nu_str):
-                    if nu_str in y:
-                        np.testing.assert_allclose(
-                            flux.getFlux(nu, E, cz),
-                            y[nu_str],
-                            rtol=1e-13,
-                            atol=1e-13,
-                        )
-                    else:
-                        np.testing.assert_array_equal(flux.getFlux(nu, E, cz), 0)
-                    assert flux.getFlux(nu, np.nextafter(emin, -np.inf), 0) == 0
-                    assert flux.getFlux(nu, np.nextafter(emax, +np.inf), 0) == 0
-                    assert flux.getFlux(nu, geomE, -1.0000000000000002) == 0
-                    assert flux.getFlux(nu, geomE, +1.0000000000000002) == 0
-            for nu in [11, 13, 15, 17, 22, 2212, 1000260560]:
-                with subtests.test(model=model, knee=knee, nu=nu):
-                    with pytest.raises(RuntimeError):
-                        flux.getFlux(nu, geomE, 0)
-                    with pytest.raises(RuntimeError):
-                        flux.getFlux(-nu, geomE, 0)
-
-
-def test_fluxes(subtests):
-    compare_fluxes(subtests, data["high_energy_data"], np.logspace(2, 9, 8))
-    compare_fluxes(subtests, data["low_energy_data"], [1.0e00, 1.0e01, 1.0e02, 1.0e03])
+@pytest.mark.parametrize(("energy", "model", "knee"), models)
+def test_flux(energy, model, knee):
+    flux = nuflux.makeFlux(model)
+    flux.knee_reweighting_model = knee
+    emin, emax = flux.energy_range
+    geom_energy = (emin * emax) ** 0.5
+    energies = low_energies if energy == "low_energy_data" else high_energies
+    eng, cz = np.meshgrid(energies, coszeniths)
+    assert model in nuflux.availableFluxes()
+    if knee != "none":
+        assert knee in nuflux.kneesForFlux(model)
+    for nu_str, nu in nuflux.ParticleType.names.items():
+        y = data[energy][model][knee].get(nu_str, 0)
+        assert_allclose(flux.getFlux(nu, eng, cz), y, rtol=1e-13, atol=1e-13)
+        assert flux.getFlux(nu, np.nextafter(emin, -np.inf), 0) == 0
+        assert flux.getFlux(nu, np.nextafter(emax, +np.inf), 0) == 0
+        assert flux.getFlux(nu, geom_energy, -1.0000000000000002) == 0
+        assert flux.getFlux(nu, geom_energy, +1.0000000000000002) == 0
+    for nu in [11, 13, 15, 17, 22, 2212, 1000260560]:
+        with pytest.raises(RuntimeError):
+            flux.getFlux(nu, geom_energy, 0)
+        with pytest.raises(RuntimeError):
+            flux.getFlux(-nu, geom_energy, 0)
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-v", __file__]))
+    pytest.main(["-v", __file__, *sys.argv[1:]])
