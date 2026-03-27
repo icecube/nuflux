a8d7a50dfa5aa9bb22092d76520a746a63c82c05
David Schultz <david.schultz@icecube.wisc.edu>
2026-03-27 15:22:58 -0500

    update the docs upload password to be more secure

--- docs/docs-upload.sh
+++ docs/docs-upload.sh
@@ -7,8 +7,8 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_REVISION=`git rev-parse --verify --short HEAD`
 TARBALL_NAME="${PROJECT_NAME}-docs-${GIT_BRANCH}-${GIT_REVISION}.tar.gz"
 
-if [ -z "${ICECUBE_PASSWORD}" ]; then
-    echo ICECUBE_PASSWORD not set, skipping upload
+if [ -z "${DOCS_PASSWORD}" ]; then
+    echo DOCS_PASSWORD not set, skipping upload
     exit 0
 fi
 
@@ -22,7 +22,7 @@ echo running $TARCMD
 ${TARCMD}
 
 URL="https://docs.icecube.aq/api/upload?path=${PROJECT_NAME}/${BRANCH_NAME}"
-UPLOADCMD="curl -XPUT -i --data-binary @${TARBALL_NAME} ${URL} -u icecube:${ICECUBE_PASSWORD}"
+UPLOADCMD="curl -XPUT -i --data-binary @${TARBALL_NAME} ${URL} -u icecube:${DOCS_PASSWORD}"
 echo running ${UPLOADCMD}
 ${UPLOADCMD}
 exit $?
